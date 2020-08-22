6f8bdf1848a9a1011d8b6d4ae77f0808c788e2ca
Rob van de Pijpekamp <info@art-and-tech.com>
2020-08-22 20:24:04 +0200

    all cllabvle feefect added for Wav.. functions

--- functions/wav_effect.h
+++ functions/wav_effect.h
@@ -9,12 +9,35 @@ public:
 
   const char* GetEffectName(BladeEffectType type) {
 	  switch (type){
-	   case EFFECT_PREON:      return "preon";
-	   case EFFECT_IGNITION:   return "poweron,out,poweronf";
-	   case EFFECT_RETRACTION: return "poweroff,in";
-	   case EFFECT_PSTOFF:     return "pstoff";
-	   case EFFECT_CLASH:      return "clash,clsh";
-	   case EFFECT_BLAST:      return "blaster,blst";
+	   case EFFECT_CLASH:      	return "clash,clsh";
+	   case EFFECT_BLAST:      	return "blaster,blst";
+	   case EFFECT_FORCE:      	return "force";
+	   case EFFECT_STAB:       	return "stab";
+	   case EFFECT_BOOT:	   	return "boot";
+	   case EFFECT_LOCKUP_BEGIN:return "bgnlock";
+	   case EFFECT_LOCKUP_END: 	return "endlock";
+	   case EFFECT_DRAG_BEGIN:	return "bgndrag";
+	   case EFFECT_DRAG_END:	return "enddrag";
+	   case EFFECT_PREON:      	return "preon";
+	   case EFFECT_PSTOFF:     	return "pstoff";
+	   case EFFECT_IGNITION:   	return "poweron,out,poweronf";//including force poweron
+	   case EFFECT_RETRACTION: 	return "poweroff,in";
+	   case EFFECT_CHANGE:	   	return "ccbegin,ccchange,ccend";
+	   case EFFECT_NEWFONT:    	return "font";
+	   
+	   case EFFECT_STUN:	   	return "stun";
+	   case EFFECT_FIRE:	   	return "none";//no effect defined
+	   case EFFECT_CLIP_IN:	   	return "clipin";
+	   case EFFECT_CLIP_OUT:   	return "clipout";
+	   case EFFECT_RELOAD:	   	return "reload";
+	   case EFFECT_MODE:	   	return "mode";
+	   case EFFECT_RANGE:	   	return "range";
+	   case EFFECT_EMPTY:	   	return "empty";
+	   case EFFECT_FULL:	   	return "full";
+	   case EFFECT_JAM:		   	return "jam";
+	   case EFFECT_UNJAM:	   	return "unjam";
+	   case EFFECT_PLI_ON:	   	return "plion";
+	   case EFFECT_PLI_OFF:	   	return "plioff";
 	  } 
 	 return "none"; 
 	  
