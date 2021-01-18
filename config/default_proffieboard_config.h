a7b5b3e5fc65c16d5119a9ebcb90d48e0c852cda
Rob van de Pijpekamp <info@art-and-tech.com>
2021-01-18 18:18:21 +0100

    Changes to be committed: 	new file:   Modifications.txt 	modified:   ProffieOS.ino 	modified:   blades/dim_blade.h 	modified:   common/saber_base.h 	modified:   config/default_proffieboard_config.h 	new file:   config/default_proffieboard_config_singleblade.h 	modified:   props/prop_base.h 	new file:   props/saber_aat_1_button.h 	modified:   sound/effect.h 	modified:   sound/hybrid_font.h
    Saber_base new members for bitghtness, volume, dim and menu change
    
    Dim blade, make it dynmaiclay changeable
    
    PropBase add functions for menu items
    
    Hybrid font added menu items for dim, volume and menu exit
    
    Sound, Effect.h, added Effects for Dim, Volume and Exit mneu.
    
    use saber_aat_1_button for example of usage and implementation of menu items
    
    Config file
    
    Add to config_top
    
    Set Blade to DimBlade;
    
    BladeConfig blades[] = {
     { 0, DimBlade(75, WS281XBladePtr<123, bladePin, Color8::GRB, PowerPINS<> >()), //Start off at 75% brightness
         CONFIGARRAY(presets) },
      };
    
    ProffieOS
    
    find the section where the SaberBases are initiated and modify to;
    
    SaberBase* saberbases = NULL;
    SaberBase::LockupType SaberBase::lockup_ = SaberBase::LOCKUP_NONE;
    SaberBase::ColorChangeMode SaberBase::color_change_mode_ =
      SaberBase::COLOR_CHANGE_MODE_NONE;
    SaberBase::VolumeChangeMode SaberBase::volume_change_mode_ =
      SaberBase::VOLUME_CHANGE_MODE_NONE;
    SaberBase::DimChangeMode SaberBase::dim_change_mode_ =
      SaberBase::DIM_CHANGE_MODE_NONE;
    SaberBase::MenuChangeMode SaberBase::menu_change_mode_ =
      SaberBase::MENU_CHANGE_MODE_NONE;
    bool SaberBase::on_ = false;
    uint32_t SaberBase::last_motion_request_ = 0;
    uint32_t SaberBase::current_variation_ = 0;
    uint32_t SaberBase::current_brightness_ = 16384; //16384 = 100%
    
    modify PROP_TYPE (can be done inf config file as well I beleive?)
    
    //#include "props/saber.h"

--- config/default_proffieboard_config.h
+++ config/default_proffieboard_config.h
@@ -12,7 +12,7 @@
 #include "proffieboard_config.h"
 #define NUM_BLADES 2
 #define NUM_BUTTONS 2
-#define VOLUME 100
+#define VOLUME 1000
 const unsigned int maxLedsPerStrip = 144;
 #define CLASH_THRESHOLD_G 1.0
 #define ENABLE_AUDIO
@@ -24,130 +24,6 @@ const unsigned int maxLedsPerStrip = 144;
 
 #ifdef CONFIG_PRESETS
 Preset presets[] = {
-	  { "common;VaderII", "tracks/main.wav",
-     StylePtr<Layers<
-		 Blue,
-		 BlastL<White>,
-		 SimpleClashL<White>,
-		 ResponsiveLockupL<Strobe<White,BrownNoiseFlicker<White,Red,300>,50,1>,
-		                   TrConcat<TrInstant,White,TrFade<200>>,TrFade<400>,
-						            Scale<BladeAngle<0,16000>,Int<10000>,Int<30000>>,Int<10000>,
-									Scale<SwingSpeed<100>,Int<10000>,Int<14000>>>,
-		 ResponsiveDragL<AudioFlicker<Blue,White>,TrInstant,TrInstant,Int<10000>,Int<16384>>,
-		 ResponsiveMeltL<Mix<TwistAngle<>,Red,Orange>,TrWipeIn<600>,TrSmoothFade<600>>,
-		 ResponsiveStabL<Orange,TrWipeIn<50>,TrWipe<50>>,BlastL<White>,
-		 ResponsiveLightningBlockL<White>,
-		 InOutTrL<  TrWipeX< Percentage<WavLen<EFFECT_IGNITION>,10> >, TrWipeInX< Percentage<WavLen<EFFECT_RETRACTION>, 5> >,Black>,
-		 
-		 TransitionEffectL<TrConcat<TrInstant,AlphaL<White,Bump<Int<0>,Int<6000>>>,
-						   TrFadeX<WavLen<EFFECT_RETRACTION>>,AlphaL<Orange,Bump<Int<0>,Int<4000>>>,
-						   TrFadeX< Percentage<WavLen<EFFECT_PSTOFF>,60> >,AlphaL<Red,Bump<Int<0>,Int<3000>>>,
-						   TrFadeX< Percentage<WavLen<EFFECT_PSTOFF>,40>> >,EFFECT_RETRACTION>,
-						   
-		 TransitionEffectL<TrConcat<TrFadeX< Percentage<WavLen<EFFECT_PREON>,125> >,AlphaL<HumpFlickerL<Orange,10>,Bump<Int<0>,Int<4000>>>,
-		                   TrFadeX< Percentage<WavLen<EFFECT_IGNITION>,50> >,AlphaL<HumpFlickerL<Orange,15>,Bump<Int<0>,Int<5000>>>,
-						   TrFadeX< Percentage<WavLen<EFFECT_IGNITION>,25> >,AlphaL<HumpFlickerL<White,20>,Bump<Int<0>,Int<6000>>>,
-						   TrBoingX< Percentage<WavLen<EFFECT_IGNITION>,15>,3>>,EFFECT_PREON>>
-		 >(),	
-    StyleNormalPtr<Green, White, 500, 1800>(),
-	"layers vader"},	
-
-	  // { "common;Portal", "tracks/main.wav",
-     // StylePtr<Layers<
-		 // Blue,
-		 // BlastL<White>,
-		 // SimpleClashL<White>,
-		 // ResponsiveLockupL<Strobe<White,BrownNoiseFlicker<White,Red,300>,50,1>,
-		                   // TrConcat<TrInstant,White,TrFade<200>>,TrFade<400>,
-						            // Scale<BladeAngle<0,16000>,Int<10000>,Int<30000>>,Int<10000>,
-									// Scale<SwingSpeed<100>,Int<10000>,Int<14000>>>,
-		 // ResponsiveDragL<AudioFlicker<Blue,White>,TrInstant,TrInstant,Int<10000>,Int<16384>>,
-		 // ResponsiveMeltL<Mix<TwistAngle<>,Red,Orange>,TrWipeIn<600>,TrSmoothFade<600>>,
-		 // ResponsiveStabL<Orange,TrWipeIn<50>,TrWipe<50>>,BlastL<White>,
-		 // ResponsiveLightningBlockL<White>,
-		 // InOutTrL<  TrWipeX< Percentage<WavLen<EFFECT_IGNITION>,10> >, TrWipeInX< Percentage<WavLen<EFFECT_RETRACTION>, 5> >,Black>,
-		 
-		 // TransitionEffectL<TrConcat<TrInstant,AlphaL<White,Bump<Int<0>,Int<6000>>>,
-						   // TrFadeX<WavLen<EFFECT_RETRACTION>>,AlphaL<Orange,Bump<Int<0>,Int<4000>>>,
-						   // TrFadeX< Percentage<WavLen<EFFECT_PSTOFF>,60> >,AlphaL<Red,Bump<Int<0>,Int<3000>>>,
-						   // TrFadeX< Percentage<WavLen<EFFECT_PSTOFF>,40>> >,EFFECT_RETRACTION>,
-						   
-		 // TransitionEffectL<TrConcat<TrFadeX< Percentage<WavLen<EFFECT_PREON>,125> >,AlphaL<HumpFlickerL<Orange,10>,Bump<Int<0>,Int<4000>>>,
-		                   // TrFadeX< Percentage<WavLen<EFFECT_IGNITION>,50> >,AlphaL<HumpFlickerL<Orange,15>,Bump<Int<0>,Int<5000>>>,
-						   // TrFadeX< Percentage<WavLen<EFFECT_IGNITION>,25> >,AlphaL<HumpFlickerL<White,20>,Bump<Int<0>,Int<6000>>>,
-						   // TrBoingX< Percentage<WavLen<EFFECT_IGNITION>,15>,3>>,EFFECT_PREON>>
-		 // >(),	
-    // StyleNormalPtr<Green, White, 500, 1800>(),
-	// "layers vader"},	
-
-    { "common;Portal", "tracks/main.wav",
-	StylePtr<Layers<Blue, // basic base layer for now
-		// whole rest of style goes here
-
-		InOutTrL<
-		// Extention - wipe out spark tip followed by 2 stage slowing stripes that swell from blue to orange then fade out
-		TrJoin<
-		   TrWipe<500>,
-		   TrWaveX<LemonChiffon,Int<1000>,Int<130>,Int<500>,Int<0>>,
-		   TrConcat<TrWipe<500>,AlphaL<Stripes<1400,-3600,Blue,Rgb<0,0,60>,Blue,Rgb<0,0,128>>,Int<32768>>,
-					TrFade<350>,AlphaL<Stripes<1400,-3000,DarkOrange,Rgb<60,17,0>,Orange,Rgb<128,34,0>>,Int<32768>>,
-					TrSmoothFade<600>>>,
-		// Retraction - wipe in 3 stage accelerating stripes followed by spark in
-		TrConcat<
-		TrWipeInX< Percentage<WavLen<EFFECT_RETRACTION>,40> >,Stripes<3000,1500,Blue,Orange>,
-		TrFadeX< Percentage<WavLen<EFFECT_RETRACTION>,20> >,        Stripes<3000,2000,Blue,Orange>,
-		TrFadeX< Percentage<WavLen<EFFECT_RETRACTION>,20> >,        Stripes<3000,2500,Blue,Orange>,
-		TrJoin<
-		  TrWipeInX< Percentage<WavLen<EFFECT_RETRACTION>,20> >,
-		  TrWaveX<LemonChiffon, Percentage<WavLen<EFFECT_RETRACTION>,100> ,Int<130>, Percentage<WavLen<EFFECT_RETRACTION>,20> ,Int<32768>> >>,
-		// Color When Off
-		Black>,
-
-		// Ignition Effects
-		TransitionEffectL<TrConcat<TrInstant,White,TrFade<50>>,EFFECT_IGNITION>,
-
-		// Ignition Emitter flare
-		TransitionEffectL<TrConcat<
-		TrInstant,AlphaL<LemonChiffon,Bump<Int<0>,Int<10000>>>,
-		TrFade<750>,AlphaL<Orange,Bump<Int<0>,Int<4000>>>,
-		TrFade<150>,AlphaL<Rgb<255,54,0>,Bump<Int<0>,Int<2800>>>,
-		TrFade<150>,AlphaL<OrangeRed,Bump<Int<0>,Int<1700>>>,
-		TrFade<200>>,EFFECT_IGNITION>,
-
-		// Retraction Emitter flare
-		MultiTransitionEffectL<TrConcat<
-		TrInstant,AlphaL<OrangeRed,Bump<Int<0>,Int<1000>>>,
-		TrFadeX< Percentage<WavLen<EFFECT_RETRACTION>,20> >,AlphaL<Rgb<255,54,0>,Bump<Int<0>,Int<3500>>>,
-		TrFadeX< Percentage<WavLen<EFFECT_RETRACTION>,20> >,AlphaL<Orange,Bump<Int<0>,Int<9000>>>,
-		//TrFadeX< Percentage<WavLen<EFFECT_RETRACTION>,30> >,AlphaL<BrownNoiseFlicker<LemonChiffon,Black,300>,Bump<Int<0>,Int<17000>>>, //Optional effect
-		TrFadeX< Percentage<WavLen<EFFECT_RETRACTION>,30> >,AlphaL<Strobe<LemonChiffon,Black,15,50>,Bump<Int<0>,Int<17000>>>,
-		TrFadeX< Percentage<WavLen<EFFECT_RETRACTION>,30> >>,EFFECT_RETRACTION,4>,
-
-		// pstoff Effects - first delay is wait for retraction retraction to finish
-		MultiTransitionEffectL<TrConcat<TrDelayX< Percentage<WavLen<EFFECT_RETRACTION>,100> >,AlphaL<White,SmoothStep<Int<15000>,Int<-20000>>>,TrFade<75>>,EFFECT_RETRACTION,4>,
-		MultiTransitionEffectL<TrConcat<TrDelayX< Percentage<WavLen<EFFECT_RETRACTION>,100> >,AlphaL<Red,Int<0>>,TrFadeX< Percentage<WavLen<EFFECT_RETRACTION>,70> >,SparkleL<EffectSequence<EFFECT_BLAST,Blue,Orange>>,TrSmoothFadeX< Percentage<WavLen<EFFECT_RETRACTION>,100> > >,EFFECT_RETRACTION,4>,
-		MultiTransitionEffectL<TrConcat<
-							  TrDelayX< Percentage<WavLen<EFFECT_RETRACTION>,100> >,AlphaL<Blue,Bump<Int<0>,Int<5000>>>,
-							  TrDelayX< Percentage<WavLen<EFFECT_PSTOFF>,16>  >,AlphaL<Orange,Bump<Int<0>,Int<4000>>>,
-							  TrDelayX< Percentage<WavLen<EFFECT_PSTOFF>,16>  >,AlphaL<Blue,Bump<Int<0>,Int<3000>>>,
-							  TrDelayX< Percentage<WavLen<EFFECT_PSTOFF>,16>  >,AlphaL<Orange,Bump<Int<0>,Int<1600>>>,
-							  TrDelayX< Percentage<WavLen<EFFECT_PSTOFF>,16>  >,AlphaL<Blue,Bump<Int<0>,Int<1000>>>,
-							  TrDelayX< Percentage<WavLen<EFFECT_PSTOFF>,16>  >,AlphaL<Orange,Bump<Int<0>,Int<500>>>,
-							  TrDelayX< Percentage<WavLen<EFFECT_PSTOFF>,16>  >>,EFFECT_RETRACTION,4>,
-		// preon
-		TransitionEffectL<TrConcat<
-							  TrInstant,                                      AlphaL<Orange,Bump<Int<0>,Int<500>>>,
-							  TrDelayX< Percentage<WavLen<EFFECT_PREON>,16> >,AlphaL<Blue,Bump<Int<0>,Int<1000>>>,
-							  TrDelayX< Percentage<WavLen<EFFECT_PREON>,16> >,AlphaL<Orange,Bump<Int<0>,Int<2000>>>,
-							  TrDelayX< Percentage<WavLen<EFFECT_PREON>,16> >,AlphaL<Blue,Bump<Int<0>,Int<3000>>>,
-							  TrDelayX< Percentage<WavLen<EFFECT_PREON>,16> >,AlphaL<Orange,Bump<Int<0>,Int<4000>>>,
-							  TrDelayX< Percentage<WavLen<EFFECT_PREON>,16> >,AlphaL<Blue,Bump<Int<0>,Int<5000>>>,
-							  TrDelayX< Percentage<WavLen<EFFECT_PREON>,16> >>,EFFECT_PREON> >>(), 	
-    StyleNormalPtr<Green, White, 500, 1800>(),
-	"layers Portal"},	
-	
-	
-	
   { "TeensySF", "tracks/venus.wav",
     StyleNormalPtr<CYAN, WHITE, 300, 800>(),
     StyleNormalPtr<CYAN, WHITE, 300, 800>(), "cyan"},
@@ -184,13 +60,15 @@ Preset presets[] = {
   { "TthCrstl", "tracks/uranus.wav",
     StyleStrobePtr<WHITE, Rainbow, 15, 300, 800>(),
     StyleStrobePtr<WHITE, Rainbow, 15, 300, 800>(), "strobe"},
+  { "TeensySF", "tracks/venus.wav",
+    &style_pov,
+    StyleNormalPtr<BLACK, BLACK, 300, 800>(), "POV"},
   { "SmthJedi", "tracks/mars.wav",
     &style_charging,
     StyleNormalPtr<BLACK, BLACK, 300, 800>(), "Battery\nLevel"}
 };
 BladeConfig blades[] = {
- { 0, //WS2811BladePtr<97, WS2811_800kHz>(),
-     WS281XBladePtr<50, bladePin, Color8::GRB, PowerPINS<> >(),
+ { 0, WS2811BladePtr<97, WS2811_800kHz>(),
      SimpleBladePtr<CreeXPE2WhiteTemplate<550>,
                     CreeXPE2BlueTemplate<240>,
                     CreeXPE2BlueTemplate<240>, NoLED, bladePowerPin4, bladePowerPin5, bladePowerPin6, -1>(),
